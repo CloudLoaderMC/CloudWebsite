3dd39ec0de7a04b28734aab91e4047a9ae24f92e
DaRubyMiner360 <darubyminer360@gmail.com>
2022-07-12 01:48:50 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/content/index.html'));
+});
+
+/*// Meta start
+
+// TODO: Link with maven
+let minecraftVersions = [
+    {
+        "version": "1.19",
+        "stable": true
+    }
+];
+
+// TODO: Link with github tags
+// TODO: Remove things like maven
+let loaderVersions = [
+    {
+        "separator": ".",
+        "build": 0,
+        "maven": "net.fabricmc:fabric-loader:0.14.8",
+        "version": "1.0.0",
+        "stable": true
+  },
+];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/meta/versions/minecraft', (req, res) => {
+    res.json(minecraftVersions);
+});
+
+app.get('/meta/versions/loader', (req, res) => {
+    res.json(loaderVersions);
+});
+
+// Meta end*/
+
+
+app.listen(port, () => {
+  console.log(`App server is running on port ${port}`);
+  console.log("to end press Ctrl + C");
+});
