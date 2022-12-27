b693a27d85f90b2c0f4181fbe4215e1bc083de75
Robert Soncrant <55484201+DaRubyMiner360@users.noreply.github.com>
2022-12-27 02:59:45 +0000

    Started actually adding some content

--- index.js
+++ index.js
@@ -17,54 +17,84 @@ app.get('*', (req, res) => {
     if (fs.existsSync(path.join(p, req.url))) {
         res.sendFile(path.join(p, req.url));
     }
-    else if (fs.existsSync(path.join(p, req.url, '.html'))) {
-        res.sendFile(path.join(p, req.url, '.html'));
+    else if (fs.existsSync(path.join(p, req.url + '.html'))) {
+        res.sendFile(path.join(p, req.url + '.html'));
     }
     else if (fs.existsSync(path.join(p, req.url + 'l'))) {
         // For .htm -> .html
         res.sendFile(path.join(p, req.url + 'l'));
     }
-    else if (fs.existsSync(path.join(p, req.url, '.htm'))) {
-        res.sendFile(path.join(p, req.url, '.htm'));
+    else if (fs.existsSync(path.join(p, req.url + '.htm'))) {
+        res.sendFile(path.join(p, req.url + '.htm'));
     }
-    else if (fs.existsSync(path.join(p, req.url, '.js'))) {
-        res.sendFile(path.join(p, req.url, '.js'));
+    else if (fs.existsSync(path.join(p, req.url + '.js'))) {
+        res.sendFile(path.join(p, req.url + '.js'));
     }
     else if (fs.existsSync(path.join(p, '/js', req.url))) {
         res.sendFile(path.join(p, '/js', req.url));
     }
-    else if (fs.existsSync(path.join(p, '/js', req.url, '.js'))) {
-        res.sendFile(path.join(p, '/js', req.url, '.js'));
+    else if (fs.existsSync(path.join(p, '/js', req.url + '.js'))) {
+        res.sendFile(path.join(p, '/js', req.url + '.js'));
     }
-    else if (fs.existsSync(path.join(p, req.url, '.css'))) {
-        res.sendFile(path.join(p, req.url, '.css'));
+    else if (fs.existsSync(path.join(p, req.url + '.css'))) {
+        res.sendFile(path.join(p, req.url + '.css'));
     }
     else if (fs.existsSync(path.join(p, '/css', req.url))) {
         res.sendFile(path.join(p, '/css', req.url));
     }
-    else if (fs.existsSync(path.join(p, '/css', req.url, '.css'))) {
-        res.sendFile(path.join(p, '/css', req.url, '.css'));
+    else if (fs.existsSync(path.join(p, '/css', req.url + '.css'))) {
+        res.sendFile(path.join(p, '/css', req.url + '.css'));
+    }
+    else if (fs.existsSync(path.join(p, req.url + '.woff2'))) {
+        res.sendFile(path.join(p, req.url + '.woff2'));
+    }
+    else if (fs.existsSync(path.join(p, '/fonts', req.url))) {
+        res.sendFile(path.join(p, '/fonts', req.url));
+    }
+    else if (fs.existsSync(path.join(p, '/fonts', req.url + '.woff2'))) {
+        res.sendFile(path.join(p, '/fonts', req.url + '.woff2'));
+    }
+    else if (fs.existsSync(path.join(p, '/fonts', req.url + '.woff'))) {
+        res.sendFile(path.join(p, '/fonts', req.url + '.woff'));
+    }
+    else if (fs.existsSync(path.join(p, '/fonts', req.url + '.tff'))) {
+        res.sendFile(path.join(p, '/fonts', req.url + '.tff'));
+    }
+    else if (fs.existsSync(path.join(p, '/fonts', req.url + '.eot'))) {
+        res.sendFile(path.join(p, '/fonts', req.url + '.eot'));
+    }
+    else if (fs.existsSync(path.join(p, '/fonts', req.url + '.css'))) {
+        res.sendFile(path.join(p, '/fonts', req.url + '.css'));
+    }
+    else if (fs.existsSync(path.join(p, '/fonts', req.url + '.svg'))) {
+        res.sendFile(path.join(p, '/fonts', req.url + '.svg'));
     }
     else if (fs.existsSync(path.join(p, '/img', req.url))) {
         res.sendFile(path.join(p, '/img', req.url));
     }
-    else if (fs.existsSync(path.join(p, req.url, '.png'))) {
-        res.sendFile(path.join(p, req.url, '.png'));
+    else if (fs.existsSync(path.join(p, req.url + '.ico'))) {
+        res.sendFile(path.join(p, req.url + '.ico'));
     }
-    else if (fs.existsSync(path.join(p, '/img', req.url, '.png'))) {
-        res.sendFile(path.join(p, '/img', req.url, '.png'));
+    else if (fs.existsSync(path.join(p, '/img', req.url + '.ico'))) {
+        res.sendFile(path.join(p, '/img', req.url + '.ico'));
     }
-    else if (fs.existsSync(path.join(p, req.url, '.jpg'))) {
-        res.sendFile(path.join(p, req.url, '.jpg'));
+    else if (fs.existsSync(path.join(p, req.url + '.png'))) {
+        res.sendFile(path.join(p, req.url + '.png'));
     }
-    else if (fs.existsSync(path.join(p, '/img', req.url, '.jpg'))) {
-        res.sendFile(path.join(p, '/img', req.url, '.jpg'));
+    else if (fs.existsSync(path.join(p, '/img', req.url + '.png'))) {
+        res.sendFile(path.join(p, '/img', req.url + '.png'));
     }
-    else if (fs.existsSync(path.join(p, req.url, '.jpeg'))) {
-        res.sendFile(path.join(p, req.url, '.jpeg'));
+    else if (fs.existsSync(path.join(p, req.url + '.jpg'))) {
+        res.sendFile(path.join(p, req.url + '.jpg'));
     }
-    else if (fs.existsSync(path.join(p, '/img', req.url, '.jpeg'))) {
-        res.sendFile(path.join(p, '/img', req.url, '.jpeg'));
+    else if (fs.existsSync(path.join(p, '/img', req.url + '.jpg'))) {
+        res.sendFile(path.join(p, '/img', req.url + '.jpg'));
+    }
+    else if (fs.existsSync(path.join(p, req.url + '.jpeg'))) {
+        res.sendFile(path.join(p, req.url + '.jpeg'));
+    }
+    else if (fs.existsSync(path.join(p, '/img', req.url + '.jpeg'))) {
+        res.sendFile(path.join(p, '/img', req.url + '.jpeg'));
     }
     else {
         res.status(404).sendFile(path.join(__dirname, '/content/404.html'));
@@ -111,6 +141,6 @@ app.get('/meta/versions/loader', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`App server is running on port ${port}`);
-  console.log("to end press Ctrl + C");
+    console.log(`App server is running on port ${port}`);
+    console.log("to end press Ctrl + C");
 });
